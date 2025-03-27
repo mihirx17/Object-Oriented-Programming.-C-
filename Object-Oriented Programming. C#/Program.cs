d2ea73b5236629e3e4fe7be5a6990a1e5732f99c
mihirx17 <mihirkumargupta2@gmail.com>
2025-03-27 22:17:28 +0530

    Important Point of Abstraction

--- Object-Oriented Programming. C#/Program.cs
+++ Object-Oriented Programming. C#/Program.cs
@@ -2,7 +2,7 @@
 abstract class Animal
 {
     protected abstract void Speak(); /// Abstract method
-    protected void Eat() => Console.WriteLine("Eating..."); /// Non-abstract method    
+    public void Eat() => Console.WriteLine("Eating..."); /// Non-abstract method    
 }
 class subclass : Animal
 {
@@ -12,8 +12,13 @@ class subclass : Animal
         Speak();
     }
 }
+
 abstract class user
 {
+    public  user()
+    {
+        Console.WriteLine("User Constructor");
+    }
     public abstract void info(); /// Abstract method
     public void work() => Console.WriteLine("Devlopment..."); /// Non-abstract method
 
@@ -27,8 +32,22 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        subclass obj = new subclass();
-        obj.Sound();
+       Animal animal = new subclass();
+        animal.Eat();
+        subclass sub = new subclass();
+        sub.Sound();
+        user u = new subclass2();
+        u.info();
+        u.work();
+
 
     }
 }
+//abstract class can have abstract and non-abstract methods
+//we can't create an object of an abstract class
+//abstract method must be implemented in the derived class
+//abstract method can't have a body
+//Non-abstract method can have a body
+// non-abstract method can be called using the object of the derived class
+//ABtract class can have a constructor
+//
