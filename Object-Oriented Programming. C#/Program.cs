cbd5acc5486403617c1ea61a30d0ab22c12dd73a
mihirx17 <mihirkumargupta2@gmail.com>
2025-03-27 22:04:12 +0530

    interface  things

--- Object-Oriented Programming. C#/Program.cs
+++ Object-Oriented Programming. C#/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿interface  Animal
+{
+    void animalSpeak(); //abstract method
+   
+}
+interface Humanbeing
+{
+    void HumanSpeak(); //abstract method
+}
+class subclass : Animal,Humanbeing
+{
+   public void  animalSpeak()
+    {
+        Console.WriteLine("Memooow");
+    }
+    public void HumanSpeak()
+    {
+        Console.WriteLine("Hiii i Am Mihir");
+    }
+
+}
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        subclass subclass = new subclass();
+        subclass.animalSpeak();
+        subclass.HumanSpeak();
+     
+    }
+}
