f61019da924626831d952e230f47bcf65f6a22cc
mihirx17 <mihirkumargupta2@gmail.com>
2025-03-27 22:09:53 +0530

    Inheriteance !!! Practice

--- Object-Oriented Programming. C#/Program.cs
+++ Object-Oriented Programming. C#/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+namespace UserModule
+{
+    public class Program
+    {
+        private class UserInformation
+        {
+            [Required]
+
+            private int UserName { get; set; }
+
+            private void Display()
+            {
+                Console.WriteLine($"User Name: {UserName}");
+            }
+            public void SetUserName(int userName)
+            {
+                UserName = userName;
+                Console.WriteLine(userName);
+            }
+        }
+        //private class Subclass : UserInformation
+        //{
+        //    public void Display()
+        //    {
+        //        Console.WriteLine($"User Name: {UserName}");
+        //    }
+
+        //}
+        public class AnotheruserInfomation
+        {
+            [Required]
+            public string? username { get; set; }
+            public void Display()
+            {
+                Console.WriteLine($"User Name: {username}");
+            }
+        }
+        public class subclass : AnotheruserInfomation
+        {
+            public void Display()
+            {
+                Console.WriteLine($"User Name: {username}");
+            }
+        }
+        public class Employee
+        {
+            protected string? username { get; set; }
+           
+        }
+        public class sub : Employee
+        {
+            public void subDisplay()
+            {
+                this.username = "Mihir";
+                Console.WriteLine($"User Name: {username}");
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            UserInformation userInformation = new UserInformation();
+            userInformation.SetUserName(1);
+            AnotheruserInfomation anotheruserInfomation = new AnotheruserInfomation();
+            //userInformation.Display();
+            anotheruserInfomation.username = "Mihir";
+            anotheruserInfomation.Display();
+            subclass subclass = new subclass();
+            subclass.username = "Mihir";
+            subclass.Display();
+            sub sub = new sub();
+                sub.subDisplay();
+        }
+    }
+}
