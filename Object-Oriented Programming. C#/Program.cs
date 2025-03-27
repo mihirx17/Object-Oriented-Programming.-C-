2f3aa54c0798fa5777c628ceb5aca0ad3057eee5
mihirx17 <mihirkumargupta2@gmail.com>
2025-03-27 22:08:48 +0530

    Abstraction Practice

--- Object-Oriented Programming. C#/Program.cs
+++ Object-Oriented Programming. C#/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+abstract class Animal
+{
+    protected abstract void Speak(); /// Abstract method
+    protected void Eat() => Console.WriteLine("Eating..."); /// Non-abstract method    
+}
+class subclass : Animal
+{
+    protected override void Speak() => Console.WriteLine("Meow"); /// Implementation of abstract method
+    public void Sound()
+    {
+        Speak();
+    }
+}
+abstract class user
+{
+    public abstract void info(); /// Abstract method
+    public void work() => Console.WriteLine("Devlopment..."); /// Non-abstract method
+
+}
+class subclass2 : user
+{
+    public override void info() => Console.WriteLine("User Info"); /// Implementation of abstract method
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        subclass obj = new subclass();
+        obj.Sound();
+
+    }
+}
